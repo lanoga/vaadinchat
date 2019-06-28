54b565b7d9a53af430ddd2a277e898f649aabb5c
benceboldi <51209057+benceboldi@users.noreply.github.com>
2019-06-28 12:22:05 +0200

    Project Created

--- src/main/java/com/example/test/spring/Application.java
+++ src/main/java/com/example/test/spring/Application.java
@@ -3,6 +3,9 @@ package com.example.test.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.UnicastProcessor;
 
 /**
  * The entry point of the Spring Boot application.
@@ -14,4 +17,15 @@ public class Application extends SpringBootServletInitializer {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    UnicastProcessor<ChatMessage> publisher(){
+        return UnicastProcessor.create();
+    }
+    @Bean
+    Flux<ChatMessage> messages(UnicastProcessor<ChatMessage> publisher) {
+        return publisher.replay(30).autoConnect();
+    }
+
 }
+
+
--- src/main/java/com/example/test/spring/MainView.java
+++ src/main/java/com/example/test/spring/MainView.java
@@ -1,22 +1,38 @@
 package com.example.test.spring;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.component.textfield.TextField;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.UnicastProcessor;
 
 @StyleSheet("frontend://styles/styles.css")
 @Route
 @PWA(name = "Project Base for Vaadin Flow with Spring", shortName = "Project Base")
+@Push
 public class MainView extends VerticalLayout {
 
-    public MainView() {
+    private final UnicastProcessor<ChatMessage> publisher;
+    private final Flux<ChatMessage> messages;
+
+    public MainView(UnicastProcessor<ChatMessage> publisher,
+                    Flux<ChatMessage> messages) {
+        this.publisher = publisher;
+        this.messages = messages;
+
         setSizeFull();
         setDefaultHorizontalComponentAlignment(Alignment.CENTER);
         addClassName("main-view");
@@ -25,6 +41,60 @@ public class MainView extends VerticalLayout {
         header.getElement().getThemeList().add("dark");
 
         add(header);
+
+        askUsername();
+    }
+
+    private String username;
+    private void askUsername() {
+        HorizontalLayout layout = new HorizontalLayout();
+        TextField usernameField = new TextField();
+        Button startButton = new Button("Start chat");
+        layout.add(usernameField, startButton);
+        add(layout);
+
+        startButton.addClickListener(click -> {
+            username = usernameField.getValue();
+            remove(layout);
+            showChat();
+        });
+    }
+
+    private void showChat(){
+        MessageList messageList = new MessageList();
+        add(messageList, createInputLayout());
+        expand(messageList);
+
+        messages.subscribe(message -> {
+            getUI().ifPresent(ui ->
+                    ui.access(() ->
+                            messageList.add(
+                                    new Paragraph(message.getFrom() + ": " +
+                                            message.getMessage())
+                            )
+                    ));
+        });
+    }
+
+    private Component createInputLayout() {
+        HorizontalLayout layout = new HorizontalLayout();
+
+        TextField messageField = new TextField();
+        Button sendButton = new Button("Send");
+        sendButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        layout.add(messageField, sendButton);
+        layout.setWidth("100%");
+        layout.expand(messageField);
+
+        sendButton.addClickListener(click -> {
+            publisher.onNext(new ChatMessage(username, messageField.getValue()));
+            messageField.clear();
+            messageField.focus();
+        });
+        messageField.focus();
+
+        return layout;
     }
 
 }
